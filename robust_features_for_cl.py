b726a335abf121d86ab130707966c3f57028fe11
vladmark <48940391+vladmark@users.noreply.github.com>
2021-01-12 12:58:58 +0200

    pruned

--- robust_features_for_cl.py
+++ robust_features_for_cl.py
@@ -9,8 +9,8 @@ https://colab.research.google.com/drive/1BrQRoMzjRNyIdG-x0H73xHmK53ulcL_g
 import torchvision
 import torch
 import pickle
-
 import matplotlib.pyplot as plt
+import os
 
 # setup device (CPU/GPU)
 if torch.cuda.is_available():
@@ -20,23 +20,26 @@ else:
 # device = torch.device('cpu')
 
 print(f"Working on device: {device}.")
-basepath="./"
+
+datapath = os.path.join("data", "imagenet")
+basepath = os.path.join("")
 
 """Imgnet premade downloader: https://towardsdatascience.com/how-to-scrape-the-imagenet-f309e02de1f4"""
 
 """https://medium.com/@staticmukesh/how-i-managed-to-delete-1-billions-files-from-my-google-drive-account-9fdc67e6aaca """
 
-# from dataset_management.tinyimgnet.tiny_imagenet_stats import tiny_imagenet_stats
-# tiny_imagenet_stats(basepath)
+# from dataset_management.task_creation import construct_tasks
+# construct_tasks(datapath, min_imgs = 450, max_imgs = 800)
 
 """## **Datasets** """
 
-from dataset_management.tinyimgnet.datasets import get_cl_dset
-from dataset_management.tinyimgnet.datasets import get_task
-from dataset_management.tinyimgnet.datasets import TinyImagenetTask
+from dataset_management.dataset_creation import get_cl_dset
+from dataset_management.dataset_creation import get_task
+from dataset_management.dataset_creation import TinyImagenetTask
+
 
 """### Getting the datasets"""
-cl_dset = get_cl_dset(basepath+"cl_t5_c15.txt")
+cl_dset = get_cl_dset(os.path.join(datapath, "cl_t7_c21.txt"))
 """
 cl_dset: a dictionary containing
         -> key 'meta': dict with keys: cls_no (classes per task), task_no (# of tasks), misc (number of misc classes)
@@ -47,24 +50,25 @@ datasets = list()
 all_classes = list()
 for task_no in range(cl_dset['meta']['task_no']):
   print(f"Showing info for task {task_no}")
-  task = get_task(cl_dset, task_no, basepath, verbose=False) #list of classes corresponding to task
+  task = get_task(cl_dset, task_no, datapath, verbose = False) #list of classes corresponding to task
   all_classes += task
-  dset_task = TinyImagenetTask(basepath+"/data/tiny-imagenet-200/train", task,
+  dset_task = TinyImagenetTask(os.path.join(datapath, "train"), task,
                         transform = torchvision.transforms.Compose([
-                          torchvision.transforms.ToTensor(),
-                          torchvision.transforms.RandomHorizontalFlip(p=0.5),
-                          torchvision.transforms.RandomVerticalFlip(p=0.5),
-                          torchvision.transforms.RandomRotation((-90,90)), #min and max degrees
-                          torchvision.transforms.GaussianBlur(kernel_size = (1,1), sigma=(0.1, 2.0)),
-                          torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.25), ratio=(0.3, 3.3)),
-                          torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                            torchvision.transforms.ToTensor(),
+                            torchvision.transforms.Resize((500,500)),
+                            torchvision.transforms.RandomHorizontalFlip(p=0.5),
+                            torchvision.transforms.RandomVerticalFlip(p=0.5),
+                            torchvision.transforms.RandomRotation((-90,90)), #min and max degrees
+                            torchvision.transforms.GaussianBlur(kernel_size = (1,1), sigma=(0.1, 2.0)),
+                            torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.25), ratio=(0.3, 3.3)),
+                            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                           ])
                         )
   datasets.append(dset_task)
   im, label = dset_task[0]
   if task_no == 0:
     print(f"We have images of shape {im.shape}")
-  show_dataset_task_sample = False
+  show_dataset_task_sample = True
   if show_dataset_task_sample:
     #(task loaded, showing 10 imgs from task)
     print(f"\n\n Showing 10 images from the dataset of task {task_no}.")
@@ -72,14 +76,14 @@ for task_no in range(cl_dset['meta']['task_no']):
     fig = plt.figure()
     axes = fig.subplots(2, 5)
     for i in range(10):
-      im, label = dset_task[500*i] #each class has 500 images
+      im, label = dset_task[i]
       axes[i//5, i%5].set_title(f"label: {label}")
       axes[i//5, i%5].imshow(im.permute(1,2,0))
     fig.set_figheight(5)
     fig.set_figwidth(5)
     fig.suptitle(f"Dataset {task_no}")
     fig.tight_layout()
-    fig.show()
+    plt.show(block = False)
     print("\n\n")
 
 # import sys
@@ -92,15 +96,16 @@ baseline_datasets = list()
 for task_no in range(cl_dset['meta']['task_no']):
   print(f"Showing info for task {task_no}")
   subset = all_classes[task_no*cl_dset['meta']['cls_no']:(task_no+1)*cl_dset['meta']['cls_no']]
-  dset_task = TinyImagenetTask(basepath+"data/tiny-imagenet-200/train", task,
+  dset_task = TinyImagenetTask(os.path.join(datapath, "train"), subset,
                         transform = torchvision.transforms.Compose([
-                          torchvision.transforms.ToTensor(),
-                          torchvision.transforms.RandomHorizontalFlip(p=0.5),
-                          torchvision.transforms.RandomVerticalFlip(p=0.5),
-                          torchvision.transforms.RandomRotation((-90,90)), #min and max degrees
-                          torchvision.transforms.GaussianBlur(kernel_size = (1,1), sigma=(0.1, 2.0)),
-                          torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.25), ratio=(0.3, 3.3)),
-                          torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                            torchvision.transforms.ToTensor(),
+                            torchvision.transforms.Resize((500, 500)),
+                            torchvision.transforms.RandomHorizontalFlip(p=0.5),
+                            torchvision.transforms.RandomVerticalFlip(p=0.5),
+                            torchvision.transforms.RandomRotation((-90,90)), #min and max degrees
+                            torchvision.transforms.GaussianBlur(kernel_size = (1,1), sigma=(0.1, 2.0)),
+                            torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.25), ratio=(0.3, 3.3)),
+                            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                           ])
                         )
   baseline_datasets.append(dset_task)
@@ -142,20 +147,18 @@ for task_no in range(cl_dset['meta']['task_no']):
 
 ##Training
 
-from Models import LeNet
-from Models import LeNet5
-from Models import ModDenseNet
-from Models import Block
-from Trainer import Trainer
-
 """Choose model to be used"""
 
 def get_model(model_type):
   if model_type == "LeNet":
+    from Models import LeNet
     model = LeNet()
   elif model_type == "LeNet5":
+    from Models import LeNet5
     model = LeNet5()
   elif model_type == "ModDenseNet":
+    from Models import ModDenseNet
+    from Models import Block
     model = ModDenseNet(block = Block, image_channels = 3, num_classes = cl_dset['meta']['cls_no'], device = device)
   elif "Resnet" in model_type:
     if model_type == "Resnet152":
@@ -188,8 +191,8 @@ model = get_model(model_type)
 
 get_back = False
 if get_back:
-    model.load_state_dict(basepath+"savedump/" + model.__class__.__name__ + '_' + str(trainer.num_epochs) + '_epochs' + '_lr' + str(
-        trainer.lr) + '_model_after_task' + str(task_no))
+    model.load_state_dict(torch.load(os.path.join(basepath,"savedump",
+                        f"{model.__class__.__name__}_{str(trainer.num_epochs)}_epochs_lr{str(trainer.lr)}_model_after_task{str(task_no)}")))
 
 
 #auxiliary
@@ -202,6 +205,8 @@ hyperparams = {
     "weight_decay": 1e-5,
     "device": device
 }
+
+from Trainer import Trainer
 
 trainer = Trainer(model, hyperparams)
 
@@ -220,7 +225,6 @@ def train_datasets(model, trainer, datasets, save_on_the_way = True, save_append
   train_loaders, test_loaders = [], []
   for task_no in range(len(datasets)):
     dset_task = datasets[task_no]
-
     #split dataset for this task
     train_dset_task, test_dset_task = torch.utils.data.random_split(dset_task, [int(len(dset_task)*0.8),int(len(dset_task)*0.2)])
 
@@ -248,37 +252,37 @@ def train_datasets(model, trainer, datasets, save_on_the_way = True, save_append
                                  lr = hyperparams['learning_rate'], weight_decay = hyperparams['weight_decay'])
   return metrics
 
-# metrics = train_datasets(model, trainer, datasets, save_on_the_way = True, save_appendix = "", start_from_task = 1)
-# with open(basepath+'savedump/'+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_overall_metrics', 'wb') as filehandle:
-#       pickle.dump(metrics, filehandle)
+if True:
+    metrics = train_datasets(model, trainer, datasets, save_on_the_way = False, save_appendix = "", start_from_task = 7)
+    with open(basepath+'savedump/'+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_overall_metrics', 'wb') as filehandle:
+          pickle.dump(metrics, filehandle)
 
-"""Training on baseline dataset"""
+    """Training on baseline dataset"""
 
-#@title Train on baseline (reinitialise model if it was already trained on nonbaseline tasks)
-reinitialise_model = True #@param {type:"boolean"}
-if reinitialise_model:
-  model = get_model(model_type)
-#MODEL & TRAINER MUST BE REINITIALISED IF ALREADY TRAINED ON GOOD TASKS
-metrics_baseline = train_datasets(model, trainer, baseline_datasets, save_on_the_way = True, save_appendix = "_baseline")
-with open(basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_overall_metrics'+"_baseline", 'wb') as filehandle:
-      pickle.dump(metrics_baseline, filehandle)
+    #@title Train on baseline (reinitialise model if it was already trained on nonbaseline tasks)
+    reinitialise_model = True  #@param {type:"boolean"}
+    if reinitialise_model:
+      model = get_model(model_type)
+    #MODEL & TRAINER MUST BE REINITIALISED IF ALREADY TRAINED ON GOOD TASKS
+    metrics_baseline = train_datasets(model, trainer, baseline_datasets, save_on_the_way = True, save_appendix = "_baseline")
+    with open(basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_overall_metrics'+"_baseline", 'wb') as filehandle:
+          pickle.dump(metrics_baseline, filehandle)
 
-"""Training on big dataset"""
-#NOT NEEDED ANYMORE
-# def train_big_dset(model, trainer, datasets):#now train on a the whole dataset, of all tasks:
-#   big_dset = torch.utils.data.ConcatDataset(datasets)
-#   train_dset, test_dset = torch.utils.data.random_split(
-#       big_dset, [int(len(big_dset)*0.8), int(len(big_dset)*0.2)])
-#   #create data loaders
-#   train_loader = [torch.utils.data.DataLoader(train_dset, batch_size=trainer.batch_size, shuffle=True, drop_last=False)] #must be list (with one element in this case)
-#   test_loader = [torch.utils.data.DataLoader(test_dset, batch_size=trainer.batch_size, shuffle=True, drop_last=False)]
-#   metrics_big_dset = trainer.train(train_loader, test_loader)
-#   import pickle
-#   with open(basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_metrics_big_dset', 'wb') as filehandle:
-#     pickle.dump(metrics_big_dset, filehandle)
-#   torch.save(model.state_dict(),
-#               basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_model_after_big_dset')
-#
-#   plot_metrics(metrics_big_dset, title = f"Metrics for combined dataset for model {model.__class__.__name__}.")
+    """Training on big dataset"""
+    #NOT NEEDED ANYMORE
+    # def train_big_dset(model, trainer, datasets):#now train on a the whole dataset, of all tasks:
+    #   big_dset = torch.utils.data.ConcatDataset(datasets)
+    #   train_dset, test_dset = torch.utils.data.random_split(
+    #       big_dset, [int(len(big_dset)*0.8), int(len(big_dset)*0.2)])
+    #   #create data loaders
+    #   train_loader = [torch.utils.data.DataLoader(train_dset, batch_size=trainer.batch_size, shuffle=True, drop_last=False)] #must be list (with one element in this case)
+    #   test_loader = [torch.utils.data.DataLoader(test_dset, batch_size=trainer.batch_size, shuffle=True, drop_last=False)]
+    #   metrics_big_dset = trainer.train(train_loader, test_loader)
+    #   import pickle
+    #   with open(basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_metrics_big_dset', 'wb') as filehandle:
+    #     pickle.dump(metrics_big_dset, filehandle)
+    #   torch.save(model.state_dict(),
+    #               basepath+"savedump/"+model.__class__.__name__+'_'+str(trainer.num_epochs)+'_epochs'+'_lr'+str(trainer.lr)+'_model_after_big_dset')
+    #
+    #   plot_metrics(metrics_big_dset, title = f"Metrics for combined dataset for model {model.__class__.__name__}.")
 
-from plotting_utils import plot_accuracies_for_model
